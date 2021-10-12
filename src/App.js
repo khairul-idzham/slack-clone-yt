e07962c5d6c560c5b68740b26e418c2f2ec130d0
Khairul Idzham <karipish@gmail.com>
2021-10-12 13:13:19 +0800

    Initialise

---
+++ src/App.js
@@ -0,0 +1,26 @@
+import React from 'react';
+import './App.css';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link
+} from "react-router-dom";
+
+function App() {
+  return (
+    <div className="App">
+      <Router>
+      <>
+        <Switch>
+          <Route path="/" exact>
+            <h1>This is the home page</h1>
+          </Route>
+        </Switch>
+      </>
+    </Router>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/firebase.js
@@ -0,0 +1,10 @@
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+    apiKey: "AIzaSyBn3M0d3P0ZAauJUXiUGo79vAR2q5BqR28",
+    authDomain: "slack-clone-yt-abc5f.firebaseapp.com",
+    projectId: "slack-clone-yt-abc5f",
+    storageBucket: "slack-clone-yt-abc5f.appspot.com",
+    messagingSenderId: "71518701397",
+    appId: "1:71518701397:web:f00c4e74bdc609ae94d1dc",
+    measurementId: "G-EQDFSRPR1L"
+  };
