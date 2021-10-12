9fa5b18f1a8596788c8b6b63a635e0ce112d2ec5
Khairul Idzham <karipish@gmail.com>
2021-10-12 15:35:55 +0800

    Styline headers

--- src/components/Header.js
+++ src/components/Header.js
@@ -2,6 +2,8 @@ import React from 'react';
 import styled from 'styled-components';
 import Avatar from '@mui/material/Avatar';
 import ScheduleIcon from '@mui/icons-material/Schedule';
+import SearchIcon from '@mui/icons-material/Search';
+import HelpOutlineIcon from '@mui/icons-material/HelpOutline';
 
 function Header() {
     return (
@@ -12,10 +14,14 @@ function Header() {
                 <ScheduleIcon />
             </HeaderLeft>
 
-            {/* Header Center */}
+            <HeaderSearch>
+                <SearchIcon />
+                <input placeholder="Search FutureLab" />
+            </HeaderSearch>
 
-            {/* Header Right */}
-
+            <HeaderRight>
+                <HelpOutlineIcon />
+            </HeaderRight>
         </HeaderContainer>
     )
 }
@@ -24,6 +30,13 @@ export default Header;
 
 const HeaderContainer = styled.div`
     display: flex;
+    position: fixed;
+    width: 100%;
+    align-items: center;
+    justify-content: space-between;
+    padding: 10px 0;
+    background-color: var(--slack-color);
+    color: white;
 `;
 
 const HeaderLeft = styled.div`
@@ -38,4 +51,37 @@ const HeaderLeft = styled.div`
     }
 `;
 
-const HeaderAvatar = styled(Avatar)``;
+const HeaderAvatar = styled(Avatar)`
+    cursor: pointer;
+
+    :hover {
+       opacity: 0.8;
+    }
+`;
+
+const HeaderSearch = styled.div`
+    flex: 0.4;
+    display: flex;
+    padding: 0 50px;
+    opacity: 1;
+    border-radius: 6px;
+    background-color: #421f44;
+    text-align: center;
+    border: 1px gray solid;
+    color: gray;
+
+    > input {
+        background-color: transparent;
+        border: none;
+        text-align: center;
+        min-width: 30vw;
+        outline: 0;
+        color: #fff;
+    }
+`;
+
+const HeaderRight = styled.div`
+    flex; 0.3;
+    display: flex;
+    margin-right: 20px;
+`;
