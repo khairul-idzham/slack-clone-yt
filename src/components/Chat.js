86e57d66f262aeb35898851c48f2c6800526bebe
Khairul Idzham <karipish@gmail.com>
2021-10-15 11:12:07 +0800

    building chatinput

--- src/components/Chat.js
+++ src/components/Chat.js
@@ -1,25 +1,42 @@
 import React from 'react';
 import styled from 'styled-components';
+import { useSelector } from 'react-redux';
+import { selectRoomId } from '../features/appSlice';
+import ChatInput from './ChatInput';
 
 import StarBorderOutlinedIcon from '@mui/icons-material/StarBorderOutlined';
 import InfoOutlinedIcon from '@mui/icons-material/InfoOutlined';
 
+
 function Chat() {
+
+    const roomId = useSelector(selectRoomId)
+
     return (
         <ChatContainer>
-            <Header>
-                <HeaderLeft>
-                    <h4>
-                        <strong>#Room-name</strong>
-                    </h4>
-                    <StarBorderOutlinedIcon />
-                </HeaderLeft>
-                <HeaderRight>
-                    <p>
-                        <InfoOutlinedIcon />Details
-                    </p>
-                </HeaderRight>
-            </Header>
+            <>
+                <Header>
+                    <HeaderLeft>
+                        <h4>
+                            <strong>#Room-name</strong>
+                        </h4>
+                        <StarBorderOutlinedIcon />
+                    </HeaderLeft>
+                    <HeaderRight>
+                        <p>
+                            <InfoOutlinedIcon /> Details
+                        </p>
+                    </HeaderRight>
+                </Header>
+
+                <ChatMessages>
+                    {/* List out messages */}
+                </ChatMessages>
+
+                <ChatInput
+                    channelId={roomId}
+                />
+            </>
         </ChatContainer>
     )
 }
@@ -31,13 +48,19 @@ const ChatContainer = styled.div`
     flex-grow: 1;
     overflow-y: scroll;
     margin-top: 60px;
-`
+`;
+
+const ChatMessages = styled.div`
+
+`;
+
 const Header = styled.div`
     display: flex;
     justify-content: space-between;
     padding: 20px;
     border-bottom: 1px solid lightgray;
-`
+`;
+
 const HeaderLeft = styled.div`
     display: flex;
     align-items: center;
@@ -45,13 +68,24 @@ const HeaderLeft = styled.div`
     > h4 {
         display: flex;
         text-transform: lowercase;
+        margin-right: 10px;
     }
 
     > h4 > .MuiSvgIcon-root {
         margin-left: 10px;
         font-size: 18px;
     }
-`
-const HeaderRight = styled.div`
+`;
 
-`
+const HeaderRight = styled.div`
+    > p {
+        display: flex;
+        align-items: center;
+        font-size: 18px;
+    }
+
+    > p > .MuiSvgIcon-root {
+        margin-right: 5px !important;
+        font-size: 16px;
+    }
+`;
